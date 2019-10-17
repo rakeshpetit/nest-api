2df4a02e4cdbaf83f915d179093bde2b5a968d2f
Rakesh <rakeshpetit@gmail.com>
2019-10-17 18:13:28 +0100

    change folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/controller/items.controller';
-import { ItemsService } from './items/service/items.service';
-import { ItemsModule } from './items/module/items.module';
+import { ItemsModule } from './modules/items/items.module';
 import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
