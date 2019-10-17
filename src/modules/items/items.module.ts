a0f3a76fd87258238c7f057210f0d590ed4f8cb4
Rakesh <rakeshpetit@gmail.com>
2019-10-17 18:18:13 +0100

    Add a middleware for logging

--- src/modules/items/items.module.ts
+++ src/modules/items/items.module.ts
@@ -1,5 +1,6 @@
+import { LoggerMiddleware } from './../../middlewares/logger.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ItemsController } from './controller/items.controller';
 import { ItemsService } from './service/items.service';
 import { ItemSchema } from './schemas/item.schema';
@@ -9,4 +10,8 @@ import { ItemSchema } from './schemas/item.schema';
   controllers: [ItemsController],
   providers: [ItemsService],
 })
-export class ItemsModule {}
+export class ItemsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('items');
+  }
+}
