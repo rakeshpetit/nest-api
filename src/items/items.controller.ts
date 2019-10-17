5186a6783961b40078cc5c922cb8a61ed19c39f7
Rakesh <rakeshpetit@gmail.com>
2019-10-17 14:30:42 +0100

    Create get post put delete endpoints for items

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Item';
+  }
+  //   @Get(':id')
+  //   findOne(@Param() param): string {
+  //     return `Item: ${param.id}`;
+  //   }
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item: ${id}`;
+  }
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+  @Put(':id')
+  update(@Param('id') id, @Body() updateItemDto: CreateItemDto): string {
+    return `Updated item: ${id} with name: ${updateItemDto.name} desc: ${updateItemDto.description}`;
+  }
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Deleted item: ${id}`;
+  }
+}
